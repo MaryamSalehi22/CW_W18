1e8f41e320954da2befc045453c7abd11d113908
maryamsalehi22148 <tiamsalehi22148@gmail.com>
2024-08-30 06:52:58 -0700

    Add project files.

---
+++ CW_W18/Controllers/BankAccountController.cs
@@ -0,0 +1,31 @@
+﻿using CW_W18.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW_W18.Controllers
+{
+    public class BankAccountController : Controller
+    {
+        private static BankAccount account = new BankAccount(1000);
+        public IActionResult Index()
+        {
+            ViewBag.Balance = account.Balance;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> WithDraw(decimal amount)
+        {
+            bool success = await account.WithDrawAsync(amount);
+            if (success)
+            {
+                ViewBag.Message = $"SuccessFully Withdraw {amount}";
+            }
+            else
+            {
+                ViewBag.Message = $"failed to Withdraw {amount}";
+
+            }
+            ViewBag.Balance = account.Balance;
+            return View("Index");
+        }
+    }
+}
---
+++ CW_W18/Models/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace CW_W18.Models
+{
+    public class BankAccount
+    {
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+        }
+        private decimal _balance;
+        public BankAccount(decimal InitialBalance)
+        {
+            _balance = InitialBalance;
+        }
+        public async Task<bool> WithDrawAsync(decimal amount)
+        {
+            return await Task.Run(() =>
+            {
+                if (_balance >= amount)
+                {
+                    _balance -= amount;
+                    return true;
+                }
+                return false;
+            });
+        }
+    }
+}
